3f3bdb7ad7919cae9f0ed5f6e9d3c2bc360583a8
Entity442 <mihailungoci87@gmail.com>
2021-10-28 00:28:18 -0700

    Added Blocks

--- src/main/java/net/Entity/kingdom/Kingdoms.java
+++ src/main/java/net/Entity/kingdom/Kingdoms.java
@@ -1,24 +1,15 @@
 package net.Entity.kingdom;
 
-import net.Entity.kingdom.block.ModBlocks;
+import net.Entity.kingdom.block.KModBlocks;
 import net.Entity.kingdom.item.ModItems;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Kingdoms.MOD_ID)
@@ -35,7 +26,7 @@ public class Kingdoms
         eventBus.addListener(this::setup);
 
         ModItems.register(eventBus);
-        ModBlocks.register(eventBus);
+        KModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/net/Entity/kingdom/block/KModBlocks.java
+++ src/main/java/net/Entity/kingdom/block/KModBlocks.java
@@ -15,22 +15,51 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.function.Supplier;
 
-public class ModBlocks {
+public class KModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Kingdoms.MOD_ID);
 
-    public static final RegistryObject<Block> CHAIR = registerBlock("chair",
-            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD).strength(10f)));
+    public static final RegistryObject<Block> OAK_CHAIR = registerBlock("oak_chair",
+            () -> new Block(BlockBehaviour.Properties.of(Material.WOOD).strength(1f)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
         return toReturn;
     }
-
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
                 new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
     }
 
     public static void register(IEventBus eventBus) {
